1990907f265047fc3307304ba323cd543e5a3a92
Abiyou <abiyousemegnew@gmail.com>
2023-09-26 23:13:31 +0300

    adding comments that shows that we create

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,5 +1,10 @@
 def main():
     print(f"Running Expense Tracker")
+    # Get user input for expense
+
+    # Write their expense into a file
+
+    # Read file and summrize expense
 
 if __name__ == "__main__":
     main()
