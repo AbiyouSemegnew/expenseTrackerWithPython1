7377214bba1acbbc4d79be76ad3dcb3e97ca1c1d
Abiyou <abiyousemegnew@gmail.com>
2023-09-26 23:20:11 +0300

    defining the function that we create

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,10 +1,20 @@
 def main():
     print(f"Running Expense Tracker")
     # Get user input for expense
+    get_user_expense()
 
     # Write their expense into a file
+    save_expense_to_file()
 
     # Read file and summrize expense
+    summarize_expenses()
+
+def get_user_expense():
+    print(f"Getting user expense")
+def save_expense_to_file():
+    print(f"Saving user expense")
+def summarize_expenses():
+    print(f"summarize user expense")
 
 if __name__ == "__main__":
     main()
